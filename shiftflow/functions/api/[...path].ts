ca3557c36bdc3ba655df5eaed2e8d1a85b090de8
aimu911563 <aimu911563@gmail.com>
2026-01-18 11:44:33 +0900

    u

--- shiftflow/functions/api/[...path].ts
+++ shiftflow/functions/api/[...path].ts
@@ -1,10 +1,9 @@
-export async function onRequest(context: any) {
+export const onRequest = async (context: any) => {
   const url = new URL(context.request.url);
 
-  // /api 以降のパスにして Workers へ転送
   const path = url.pathname.replace(/^\/api/, "") || "/";
   const apiUrl = `https://shiftflow-api.aimu911563.workers.dev${path}${url.search}`;
 
   const req = new Request(apiUrl, context.request);
   return fetch(req);
-}
+};
